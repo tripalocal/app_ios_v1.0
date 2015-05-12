545b4704b27b24e3e1e7cee7b0af535413ce79bd
hfz1991 <funzho@gmail.com>
2015-05-12 16:03:27 +1000

    Initial Commit

---
+++ TripalocalBeta/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TripalocalBeta
+//
+//  Created by Charles He on 12/05/2015.
+//  Copyright (c) 2015 Tripalocal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
