8f8b8a671c80b9c37930edf9a3f9612635152ecc
hfz1991 <funzho@gmail.com>
2015-05-26 16:28:28 +1000

    Received all the data from server and ready to display in the Search View

--- TripalocalBeta/Controller/TLSearchViewController.m
+++ TripalocalBeta/Controller/TLSearchViewController.m
@@ -7,22 +7,71 @@
 //
 
 #import "TLSearchViewController.h"
-
 @interface TLSearchViewController ()
 
 @end
 
-@implementation TLSearchViewController
+@implementation TLSearchViewController{
+    NSURLConnection *connection;
+    NSMutableData *jsonData;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    NSString *post = [NSString stringWithFormat:@"{\"start_datetime\":\"2015-05-08\", \"end_datetime\":\"2015-05-24\", \"city\":\"melbourne\", \"guest_number\":\"2\", \"keywords\":\"Food&Wine,Education,History&Culture\"}"];
+    NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+    NSString *postLength = [NSString stringWithFormat:@"%lu", (unsigned long)[postData length]];
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setURL:[NSURL URLWithString:@"https://www.tripalocal.com/service_search/"]];
+    [request setHTTPMethod:@"POST"];
+    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+    [request setHTTPBody:postData];
+    
+    NSURLResponse *requestResponse;
+    NSData *requestHandler = [NSURLConnection sendSynchronousRequest:request returningResponse:&requestResponse error:nil];
+    
+    NSString *requestReply = [[NSString alloc] initWithBytes:[requestHandler bytes] length:[requestHandler length] encoding:NSASCIIStringEncoding];
+    NSLog(@"requestReply: %@", requestReply);
+    
+    connection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+    if (connection) {
+        if (jsonData==NULL) {
+            jsonData=[[NSMutableData alloc]init];
+        }
+    }
+   
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+-(void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
+{
+    [jsonData setLength:0];
+}
+
+-(void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+    [jsonData appendData:data];
+}
+
+-(void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+    NSLog(@"Error:(Search)Failed with connection error.");
+}
+
+-(void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    NSLog(@"Connection Did Finish Loading.");
+    NSDictionary *allDataDictionary=[NSJSONSerialization JSONObjectWithData:jsonData options:0 error:nil];
+    
+
+}
+
 
 /*
 #pragma mark - Navigation
