6f62a87e15d212e28473e303fc913914ece651d4
hfz1991 <funzho@gmail.com>
2015-06-27 10:54:42 +1000

    TL Detail View Controller

---
+++ TripalocalBeta/Controller/TLDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  TLDetailViewController.h
+//  TripalocalBeta
+//
+//  Created by Charles He on 27/06/2015.
+//  Copyright (c) 2015 Tripalocal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TLDetailViewController : UIViewController
+
+@end
