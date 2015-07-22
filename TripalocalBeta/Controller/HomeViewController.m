c72c8910af71cad2d6cf9c4e97c551e80f1c9d5b
Ye He <yehe01@gmail.com>
2015-07-22 10:25:01 +1000

    UI fix
    change tabbar item image.

--- TripalocalBeta/Controller/HomeViewController.m
+++ TripalocalBeta/Controller/HomeViewController.m
@@ -20,38 +20,29 @@
 
     UITabBar *tabBar = self.tabBar;
     UITabBarItem *tabBarItem1 = [tabBar.items objectAtIndex:0];
-//    UITabBarItem *tabBarItem2 = [tabBar.items objectAtIndex:1];
-//    UITabBarItem *tabBarItem3 = [tabBar.items objectAtIndex:2];
-    UITabBarItem *tabBarItem3 = [tabBar.items objectAtIndex:1];
-    UITabBarItem *tabBarItem4 = [tabBar.items objectAtIndex:2];
-    
-//    tabBar.tintColor=[UIColor whiteColor];
-//    tabBar.translucent = NO;
+    UITabBarItem *tabBarItem2 = [tabBar.items objectAtIndex:1];
+    UITabBarItem *tabBarItem3 = [tabBar.items objectAtIndex:2];
+
     
     tabBarItem1.imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
-    tabBarItem1.selectedImage = [[UIImage imageNamed:@"home_s.png"]  imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal ];
+#ifdef CN_VERSION
+    tabBarItem1.selectedImage = [[UIImage imageNamed:@"ben.png"]  imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal ];
+    tabBarItem1.image = [[UIImage imageNamed:@"ben.png"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal ];
+#else
+    tabBarItem1.selectedImage = [[UIImage imageNamed:@"home.png"]  imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal ];
     tabBarItem1.image = [[UIImage imageNamed:@"home.png"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal ];
+#endif
     tabBarItem1.title = nil;
     
-//    tabBarItem2.imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
-//    tabBarItem2.selectedImage = [[UIImage imageNamed:@"search_s.png"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal ];
-//    tabBarItem2.image = [[UIImage imageNamed:@"search.png"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal ];
-//    tabBarItem2.title =nil;
-//
-//    tabBarItem3.imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
-//    tabBarItem3.selectedImage = [[UIImage imageNamed:@"heart.png"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal ];
-//    tabBarItem3.image = [[UIImage imageNamed:@"heart.png"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal ];
-//    tabBarItem3.title = nil;
+    tabBarItem2.imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
+    tabBarItem2.selectedImage = [[UIImage imageNamed:@"mytrip_s.png"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal ];
+    tabBarItem2.image = [[UIImage imageNamed:@"mytrip.png"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal ];
+    tabBarItem2.title = nil;
     
     tabBarItem3.imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
-    tabBarItem3.selectedImage = [[UIImage imageNamed:@"mytrip_s.png"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal ];
-    tabBarItem3.image = [[UIImage imageNamed:@"mytrip.png"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal ];
+    tabBarItem3.selectedImage = [[UIImage imageNamed:@"myprofile_s.png"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal ];
+    tabBarItem3.image = [[UIImage imageNamed:@"myprofile.png"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal ];
     tabBarItem3.title = nil;
-    
-    tabBarItem4.imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
-    tabBarItem4.selectedImage = [[UIImage imageNamed:@"myprofile_s.png"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal ];
-    tabBarItem4.image = [[UIImage imageNamed:@"myprofile.png"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal ];
-    tabBarItem4.title = nil;
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(presentSmsVerifiIfNotLoggedIn)
      
