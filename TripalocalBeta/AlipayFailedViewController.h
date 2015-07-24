9393c6f1e968df2d477d28f15aa0aef3d437db11
Ye He <yehe01@gmail.com>
2015-07-24 11:13:08 +1000

    Move images to catalog.

---
+++ TripalocalBeta/AlipayFailedViewController.h
@@ -0,0 +1,13 @@
+//
+//  AlipayFailedViewController.h
+//  TripalocalBeta
+//
+//  Created by Ye He on 24/07/2015.
+//  Copyright (c) 2015 Tripalocal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlipayFailedViewController : UIViewController
+
+@end
