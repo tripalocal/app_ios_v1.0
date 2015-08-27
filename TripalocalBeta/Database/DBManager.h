3abd694fd9ca2e0e9ae144d4fb02adb1e1116d14
Song Xue <carl.xs23@gmail.com>
2015-08-28 00:06:15 +1000

    Add database utilities. Add methods to run sql query on database

---
+++ TripalocalBeta/Database/DBManager.h
@@ -0,0 +1,18 @@
+//
+//  DBManager.h
+//  TripalocalBeta
+//
+//  Created by 嵩薛 on 27/08/2015.
+//  Copyright (c) 2015 Tripalocal. All rights reserved.
+//
+#import <Foundation/Foundation.h>
+
+@interface DBManager : NSObject
+
+-(instancetype)initWithDatabaseFileName: (NSString *)dbFileName;
+
+@property (nonatomic, strong) NSMutableArray *arrColumnNames;
+@property (nonatomic) int affectedRows;
+@property (nonatomic) long long lastInsertedRowID;
+
+@end
