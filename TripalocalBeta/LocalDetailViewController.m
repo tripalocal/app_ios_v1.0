e64b1c21018137f989876a81022d555839d3e089
Ye He <yehe01@gmail.com>
2015-10-16 11:31:56 +1100

    Add new detail sections.

--- TripalocalBeta/LocalDetailViewController.m
+++ TripalocalBeta/LocalDetailViewController.m
@@ -23,9 +23,10 @@
 #import "Mixpanel.h"
 #import <SecureNSUserDefaults/NSUserDefaults+SecureAdditions.h>
 #import "DetailDescTableViewController.h"
+#import "TitleDescCell.h"
+#import "SectionDescViewController.h"
 
-@interface LocalDetailViewController ()
-{
+@interface LocalDetailViewController () {
     NSString *language;
     NSString *duration;
     NSString *title;
@@ -37,12 +38,6 @@
     NSString *PREreviewerImageURL;
     NSString *reviewerImageURL;
     NSString *reviewComment;
-    NSString *highlights;
-    NSString *tips;
-    NSString *service;
-    NSString *dollarsign;
-    NSString *currency;
-    NSString *city;
     JGProgressHUD *HUD;
     NSArray *dynamicPriceArray;
     NSNumber *maxGuestNum;
@@ -55,6 +50,7 @@
     UIImage *nextPageCoverImage;
 }
 
+@property (strong, nonatomic) NSString *sectionDescription;
 @end
 
 @implementation LocalDetailViewController
@@ -100,13 +96,12 @@
     reviewComment = @"";
     reviewerImageURL = @"";
     
-    self.cellHeights = [@[@320, @240, @385, @164, @320] mutableCopy];
+    self.cellHeights = [@[@320, @240, @385, @164, @55, @55, @55, @55, @55, @55, @55] mutableCopy];
     _myTable.delegate = self;
     _myTable.dataSource = self;
     
     reviews = [[NSArray alloc] init];
     expData = [[NSDictionary alloc] init];
-    [_myTable setSeparatorStyle:UITableViewCellSeparatorStyleNone];
     
     [self mpTrackViewExperience];
     
@@ -142,14 +137,6 @@
                 language = [self transformLanugage:expData[@"experience_language"]];
                 duration = [expData[@"experience_duration"] stringValue];
                 title = expData[@"experience_title"];
-                currency = expData[@"experience_currency"];
-                dollarsign = expData[@"experience_dollarsign"];
-                city = expData[@"experience_city"];
-                highlights = expData[@"highlights"];
-                tips = expData[@"tips"];
-                service = expData[@"service"];
-                description = expData[@"description"];
-
                 reviews = expData[@"experience_reviews"];
                 nReviews = [@([reviews count]) stringValue];
                 rate = [expData[@"experience_rate"] stringValue];
@@ -234,17 +221,22 @@
     static NSString *cellIdentifier1=@"cell1";
     TLDetailTableViewCell1 *cell1=(TLDetailTableViewCell1 *) [tableView dequeueReusableCellWithIdentifier:cellIdentifier1];
     
-//    static NSString *cellIdentifier2=@"cell2";
-//    TLDetailTableViewCell2 *cell2=(TLDetailTableViewCell2 *) [tableView dequeueReusableCellWithIdentifier:cellIdentifier2];
+    static NSString *cellIdentifier2=@"cell2";
+    TLDetailTableViewCell3 *cell2=(TLDetailTableViewCell3 *) [tableView dequeueReusableCellWithIdentifier:cellIdentifier2];
     
     static NSString *cellIdentifier3=@"cell3";
-    TLDetailTableViewCell3 *cell3=(TLDetailTableViewCell3 *) [tableView dequeueReusableCellWithIdentifier:cellIdentifier3];
-    
+    TLDetailTableViewCell4 *cell3=(TLDetailTableViewCell4 *) [tableView dequeueReusableCellWithIdentifier:cellIdentifier3];
+
     static NSString *cellIdentifier4=@"cell4";
-    TLDetailTableViewCell4 *cell4=(TLDetailTableViewCell4 *) [tableView dequeueReusableCellWithIdentifier:cellIdentifier4];
+    TitleDescCell *sectionTitleCell=(TitleDescCell *) [tableView dequeueReusableCellWithIdentifier:cellIdentifier4];
     
-    static NSString *cellIdentifier5=@"cell5";
-    TLDetailTableViewCell5 *cell5=(TLDetailTableViewCell5 *) [tableView dequeueReusableCellWithIdentifier:cellIdentifier5];
+    NSString *schedule = expData[@"schedule"];
+    NSString *tips = [expData[@"tips"] stringByAppendingString:expData[@"notice"]];
+    NSString *whatsincluded = expData[@"whatsincluded"];
+    NSString *pickupDetail = expData[@"pickup_detail"];
+    NSString *disclaimer = expData[@"disclaimer"];
+    NSString *refundPolicy = expData[@"refund_policy"];
+    NSString *insurance = expData[@"insurance"];
     
     switch (indexPath.row) {
         case 0: {
@@ -272,9 +264,9 @@
                                       }];
             
             cell.languageLabel.text = language;
-            cell.currencyLabel.text = [dollarsign stringByAppendingString:currency];
+            cell.currencyLabel.text = [expData[@"experience_dollarsign"] stringByAppendingString:expData[@"experience_currency"]];
             cell.priceLabel.text = _expPrice;
-            cell.cityLabel.text = city;
+            cell.cityLabel.text = expData[@"experience_city"];
             cell.durationLabel.text = [NSString stringWithFormat:NSLocalizedString(@"local_exp_detail_per_person_for", nil), duration];
             
             return cell;
@@ -287,65 +279,138 @@
             
             cell1.parentView = self.myTable;
             cell1.selectionStyle = UITableViewCellSelectionStyleNone;
-            cell1.expDescriptionLabel.text = description;
+            cell1.expDescriptionLabel.text = expData[@"description"];
             return cell1;
         case 2:
         {
-            if(!cell3)
+            if(!cell2)
             {
-                cell3=[[TLDetailTableViewCell3 alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier3];
+                cell2=[[TLDetailTableViewCell3 alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier2];
             }
             
-            cell3.selectionStyle = UITableViewCellSelectionStyleNone;
+            cell2.selectionStyle = UITableViewCellSelectionStyleNone;
             if ([nReviews intValue] > 0) {
-                cell3.countLabel.text = [NSString stringWithFormat:NSLocalizedString(@"n_reviews", nil), nReviews];
-                cell3.reviewStars.rating = [rate floatValue];
-                cell3.reviewerName.text = [NSString stringWithFormat:@"%@ %@", reviewerFirstName, reviewerLastName];
-                cell3.commentLabel.text = reviewComment;
+                cell2.countLabel.text = [NSString stringWithFormat:NSLocalizedString(@"n_reviews", nil), nReviews];
+                cell2.reviewStars.rating = [rate floatValue];
+                cell2.reviewerName.text = [NSString stringWithFormat:@"%@ %@", reviewerFirstName, reviewerLastName];
+                cell2.commentLabel.text = reviewComment;
                 
-                [cell3.reviewerImage sd_setImageWithURL:[NSURL URLWithString:reviewerImageURL]
+                [cell2.reviewerImage sd_setImageWithURL:[NSURL URLWithString:reviewerImageURL]
                                        placeholderImage:[UIImage imageNamed:@"default_profile_image.png"]
                                                 options:SDWebImageRefreshCached];
             }else{
-                cell3.hidden = YES;
+                cell2.hidden = YES;
             }
-            return cell3;
+            return cell2;
         }
         case 3:
         {
-            if(!cell4) {
-                cell4=[[TLDetailTableViewCell4 alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier4];
+            if(!cell3) {
+                cell3=[[TLDetailTableViewCell4 alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier3];
             }
             
             NSString *coverImageURL = [NSString stringWithFormat:@"%@thumbnails/experiences/experience%@_1.jpg", [URLConfig imageServiceURLString], _experience_id_string];
             __block UIActivityIndicatorView *activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
-            activityIndicator.center = cell4.coverImage.center;
+            activityIndicator.center = cell3.coverImage.center;
             activityIndicator.hidesWhenStopped = YES;
             
-            [cell4.coverImage sd_setImageWithURL:[NSURL URLWithString:coverImageURL]
+            [cell3.coverImage sd_setImageWithURL:[NSURL URLWithString:coverImageURL]
                                 placeholderImage:nil
                                          options:SDWebImageRefreshCached
                                        completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
                                            [activityIndicator removeFromSuperview];
                                        }];
             
-            [cell4.coverImage addSubview:activityIndicator];
+            [cell3.coverImage addSubview:activityIndicator];
             [activityIndicator startAnimating];
             
-            return cell4;
+            return cell3;
         }
         case 4: {
-            if(!cell5)
-            {
-                cell5=[[TLDetailTableViewCell5 alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier5];
+            if ([schedule length] == 0) {
+                sectionTitleCell.hidden = YES;
+                self.cellHeights[4] = @(0);
+            } else {
+                sectionTitleCell.hidden = NO;
+                sectionTitleCell.sectionTitleLabel.text = @"Schedule";
+                sectionTitleCell.sectionButton.tag = 4;
+            }
+
+            return sectionTitleCell;
+        }
+        case 5: {
+            if ([tips length] == 0) {
+                sectionTitleCell.hidden = YES;
+                self.cellHeights[5] = @(0);
+            } else {
+                sectionTitleCell.hidden = NO;
+                sectionTitleCell.sectionTitleLabel.text = @"Tips";
+                sectionTitleCell.sectionButton.tag = 5;
             }
             
-            cell5.foodLabel.text = [cell5.foodLabel.text stringByAppendingFormat:@": %@", foodString];
-            cell5.ticketLabel.text = [cell5.ticketLabel.text stringByAppendingFormat:@": %@", ticketString];
-            cell5.transportLabel.text = [cell5.transportLabel.text stringByAppendingFormat:@": %@", transportString];
-            
-            return cell5;
+            return sectionTitleCell;
         }
+        case 6: {
+            if ([whatsincluded length] == 0) {
+                sectionTitleCell.hidden = YES;
+                self.cellHeights[6] = @(0);
+            } else {
+                sectionTitleCell.hidden = NO;
+                sectionTitleCell.sectionTitleLabel.text = @"What's included";
+                sectionTitleCell.sectionButton.tag = 6;
+            }
+            
+            return sectionTitleCell;
+        }
+        case 7: {
+            if ([pickupDetail length] == 0) {
+                sectionTitleCell.hidden = YES;
+                self.cellHeights[7] = @(0);
+            } else {
+                sectionTitleCell.hidden = NO;
+                sectionTitleCell.sectionTitleLabel.text = @"Pick up detail";
+                sectionTitleCell.sectionButton.tag = 7;
+            }
+            
+            return sectionTitleCell;
+        }
+        case 8: {
+            if ([disclaimer length] == 0) {
+                sectionTitleCell.hidden = YES;
+                self.cellHeights[8] = @(0);
+            } else {
+                sectionTitleCell.hidden = NO;
+                sectionTitleCell.sectionTitleLabel.text = @"Disclaimer";
+                sectionTitleCell.sectionButton.tag = 8;
+            }
+            
+            return sectionTitleCell;
+        }
+        case 9: {
+            if ([refundPolicy length] == 0) {
+                sectionTitleCell.hidden = YES;
+                self.cellHeights[9] = @(0);
+            } else {
+                sectionTitleCell.hidden = NO;
+                sectionTitleCell.sectionTitleLabel.text = @"Refund Policy";
+                sectionTitleCell.sectionButton.tag = 9;
+            }
+            
+            return sectionTitleCell;
+        }
+        case 10: {
+            if ([insurance length] == 0) {
+                sectionTitleCell.hidden = YES;
+                self.cellHeights[10] = @(0);
+            } else {
+                sectionTitleCell.hidden = NO;
+                sectionTitleCell.sectionTitleLabel.text = @"Insurance";
+                sectionTitleCell.sectionButton.tag = 10;
+            }
+
+            return sectionTitleCell;
+        }
+        
         default:
             return cell;
     }
@@ -355,7 +420,7 @@
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 5;
+    return [self.cellHeights count];
 }
 
 
@@ -395,6 +460,37 @@
     }
 }
 
+
+- (IBAction)viewSectionDescription:(UIButton *)sender {
+    switch (sender.tag) {
+        case 4:
+            self.sectionDescription = expData[@"schedule"];
+            break;
+        case 5:
+            self.sectionDescription = expData[@"tips"];
+            break;
+        case 6:
+            self.sectionDescription = expData[@"whatsincluded"];
+            break;
+        case 7:
+            self.sectionDescription = expData[@"pickup_detail"];
+            break;
+        case 8:
+            self.sectionDescription = expData[@"disclaimer"];
+            break;
+        case 9:
+            self.sectionDescription = expData[@"refund_policy"];
+            break;
+        case 10:
+            self.sectionDescription = expData[@"insurance"];
+            break;
+        default:
+            break;
+    }
+    
+    [self performSegueWithIdentifier:@"view_section_desc" sender:self];
+}
+
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
     if ([segue.identifier isEqualToString:@"checkoutSegue"]) {
         CheckoutViewController *vc=[segue destinationViewController];
@@ -414,9 +510,12 @@
         vc.reviews = reviews;
     } else if ([segue.identifier isEqualToString:@"view_detail_description"]) {
         DetailDescTableViewController *vc = [segue destinationViewController];
-        vc.description_detail = description;
-        vc.service = service;
-        vc.highlights = highlights;
+        vc.description_detail = expData[@"description"];
+        vc.service = expData[@"service"];
+        vc.highlights = expData[@"highlights"];
+    } else if ([segue.identifier isEqualToString:@"view_section_desc"]) {
+        SectionDescViewController *vc = [segue destinationViewController];
+        vc.sectionDescription = self.sectionDescription;
     }
     
 }
---
+++ TripalocalBeta/TitleDescCell.m
@@ -0,0 +1,23 @@
+//
+//  TitleDescCell.m
+//  TripalocalBeta
+//
+//  Created by Ye He on 16/10/2015.
+//  Copyright (c) 2015 Tripalocal. All rights reserved.
+//
+
+#import "TitleDescCell.h"
+
+@implementation TitleDescCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
